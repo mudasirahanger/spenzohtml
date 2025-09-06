33824a180e7545b5cdfd8c9eebe218d72c1d45a6
mudasirahanger <mudasir.ahanger@digitalindia.gov.in>
2025-09-06 11:00:35 +0530

    next js setup

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
